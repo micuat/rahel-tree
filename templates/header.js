b5cc89331cf031e52717e5390e43ba796f033fad
Glitch (glitch-in-bio) <none>
2025-03-18 12:40:01 +0000

    🏔⛷ Checkpoint ./public/styles/themes/custom-theme.css:9838890/107 ./settings.json:9838890/2 ./templates/header.js:9838890/193

--- templates/header.js
+++ templates/header.js
@@ -1,9 +1,9 @@
 import { html } from "lighterhtml";
 import settings from "../settings.json";
 
+    // <img src='${settings.avatarImage}' class='avatar' alt='${settings.name}' />
 export const renderHeader = () => html`<div>
   <div class='avatar-container'>
-    <img src='${settings.avatarImage}' class='avatar' alt='${settings.name}' />
   </div>
   <h1>${settings.name}</h1>
   <h2>${settings.metaDescription}</h2>
